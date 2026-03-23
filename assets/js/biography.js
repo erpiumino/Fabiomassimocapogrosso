aeaf2d74a96480d08a43ea82973257f102e9479b
erpiumino <118480890+erpiumino@users.noreply.github.com>
2026-03-23 12:09:45 +0000

    Problema sul fetch

--- assets/js/biography.js
+++ assets/js/biography.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {
 
       // Sezione 1
-      document.getElementById("title1").textContent = data.title1;
-      document.getElementById("subtitle1").textContent = data.subtitle1;
+      document.getElementById("title1").innerHTML = data.title1;
+      document.getElementById("subtitle1").innerHTML = data.subtitle1;
       document.getElementById("img1").src = data.img1;
 
       // Sezione 2
-      document.getElementById("title2").textContent = data.title2;
-      document.getElementById("subtitle2").textContent = data.subtitle2;
+      document.getElementById("title2").innerHTML = data.title2;
+      document.getElementById("subtitle2").innerHTML = data.subtitle2;
       document.getElementById("img2").src = data.img2;
 
     })
